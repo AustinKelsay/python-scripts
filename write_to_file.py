2e1667cf3228e44552f031ee539ee691f5cb8e88
Austin Kelsay <austinkelsay@Austins-MBP.attlocal.net>
2021-05-09 10:34:42 -0500

    Error case

--- write_to_file.py
+++ write_to_file.py
@@ -1,3 +1,18 @@
 import json
 
 # This script writes json data to a new file
+
+"""
+Args:
+:data: 'dict or iterable data to save to file'
+:filename: 'str' name of the file you will save
+:option: 'str' 'send in either "w" for write or "a" for append to the file'
+:multiline: 'bool' 'true if data needs to be iterated through with a new line'
+"""
+def write_to_file(data, filename, option="w", multiline=True)
+
+OPTIONS = ["w", "a"]
+# if an incorrect input is given
+if option.lower() not in OPTIONS:
+		print(f"Writing Method of {method} not valid. Defaulting to `{DEFAULT_METHOD}")
+		method = DEFAULT_METHOD
