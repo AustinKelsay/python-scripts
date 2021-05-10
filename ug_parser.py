588cb0f13b0acb897d66cbd5632ccc8de4e650c5
Austin Kelsay <austinkelsay@ip-192-168-1-140.us-west-2.compute.internal>
2021-05-10 15:14:39 -0500

    Script is working just need to fix output

--- ug_parser.py
+++ ug_parser.py
@@ -7,5 +7,15 @@ with open('ug_data_merged.json') as file:
 		ticket = json.loads(obj)
 		ticketlist.append(ticket)
 
-for t in ticketlist:
-	print(t['subject'])
+unique_tickets = set()
+output_tickets = []
+
+for i in range(0, len(ticketlist) -1):
+	# check if the ticket description is unique and if it has the product subject
+	if ticketlist[i]["description"] not in unique_tickets and ticketlist[i]["subject"] is not "Product Submission":
+		unique_tickets.add(ticketlist[i]["description"])
+		output_tickets.append(ticketlist[i])
+
+open("updated-file.json", "w").write(
+    json.dumps(output_tickets, sort_keys=True, indent=4, separators=(',', ': '))
+)
